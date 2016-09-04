81e7fceef986c18f4df9d9eaac2d465153afad2b
Satyam <satya12100@hotmail.com>
2016-09-04 22:11:43 +0530

    fare

--- application/models/fare_model.php
+++ application/models/fare_model.php
@@ -13,7 +13,7 @@ class Fare_model extends CI_Model{
 		return 1;
 	}
 
-	function get_all_organizations()
+	function get_all_fares()
 	{
 		
 		$sql="select a.float_fare as fare,b.txt_location as source,c.txt_location as destination from tab_fare as a join tab_locations as b ON a.int_source=b.int_location_id join tab_locations as c ON a.int_destination=b.int_location_id";
@@ -23,14 +23,14 @@ class Fare_model extends CI_Model{
 	}
 	
 
-	function delete_organization($id)
+	function delete_fare($id)
 	{
 		$sql="delete from tab_fare where int_fare_id=".$id."";
 		$query=$this->db->query($sql);
 		return $query;
 	}
 	
-	function get_organization_details($id)
+	function get_fare_details($id)
 	{
 		$sql="select * from tab_fare where int_fare_id=".$id."";
 		$query=$this->db->query($sql);
@@ -38,7 +38,7 @@ class Fare_model extends CI_Model{
 		return $result;
 	}
 	
-	function update_org($data)
+	function update_fare($data)
 	{
 		$sql_product="update tab_fare set float_fare='".$data['fare']."' where int_fare_id='".$data['fare_id']."'";
 		$query=$this->db->query($sql_product);
