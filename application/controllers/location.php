7c28c41338cbc6fa45606b828048afccea0180c1
Satyam <satya12100@hotmail.com>
2016-09-04 14:52:54 +0530

    location

---
+++ application/controllers/location.php
@@ -0,0 +1,99 @@
+<?php
+class Location extends CI_Controller{
+
+	function Location(){
+		parent::__construct();
+		$this->load->database();
+		$this->load->model('location_model');
+		error_reporting(E_ALL ^ (E_NOTICE | E_WARNING));
+		header('Access-Control-Allow-Origin: *');
+	}
+
+	function add()
+
+	{
+
+		$user=$this->session->userdata('user');
+
+		if(isset($user['int_user_id']) && $user['int_user_id']!='')
+
+		{
+
+			$data["page"]="add_location";
+
+			$this->load->view('page',$data);	
+
+		}
+
+		else
+
+		{
+
+			$this->load->view('login');	
+
+		}
+
+	}
+
+
+
+	function save()
+
+	{
+
+		$data=$this->input->post();
+
+		$user=$this->session->userdata('user');
+		
+		$data['user']=$user['int_user_id'];
+		$data['org_id']=$user['int_organization_id'];
+		$status=$this->location_model->save($data);
+
+		redirect('location/location_list', 'refresh');
+
+	}
+
+	
+
+	function location_list()
+
+	{
+
+		$user=$this->session->userdata('user');
+
+		if(isset($user['int_user_id']) && $user['int_user_id']!='')
+
+		{
+
+			$data["page"]="location_list";
+
+			$data["staff"]=$this->location_model->get_all_locations();
+			$this->load->view('page',$data);	
+
+		}
+
+		else
+
+		{
+
+			$this->load->view('login');	
+
+		}	
+
+	}
+
+
+
+	function delete()
+
+	{
+
+		$data=$this->input->get();
+
+		$this->location_model->delete_location($data['id']);
+
+		redirect('location/location_list', 'refresh');
+
+	}
+}
+?>
