626120aa104f32bbde8c630b3a8a8999f981745c
Satyam <satya12100@hotmail.com>
2016-09-04 21:51:33 +0530

    fare

--- application/models/fare_model.php
+++ application/models/fare_model.php
@@ -8,7 +8,7 @@ class Fare_model extends CI_Model{
 
 	function save($data)
 	{
-		$sql_product="insert into tab_fares values(DEFAULT,'".$data['source']."','".$data['destination']."','".$data['fare']."')";
+		$sql_product="insert into tab_fare values(DEFAULT,'".$data['source']."','".$data['destination']."','".$data['fare']."')";
 		$query=$this->db->query($sql_product);
 		return 1;
 	}
@@ -16,7 +16,7 @@ class Fare_model extends CI_Model{
 	function get_all_organizations()
 	{
 		
-		$sql="select a.float_fare as fare,b.txt_location as source,c.txt_location as destination from tab_fares as a join tab_locations as b ON a.int_source=b.int_location_id join tab_locations as c ON a.int_destination=b.int_location_id";
+		$sql="select a.float_fare as fare,b.txt_location as source,c.txt_location as destination from tab_fare as a join tab_locations as b ON a.int_source=b.int_location_id join tab_locations as c ON a.int_destination=b.int_location_id";
 		$query=$this->db->query($sql);
 		$result=$query->result_array();
 		return $result;
@@ -25,14 +25,14 @@ class Fare_model extends CI_Model{
 
 	function delete_organization($id)
 	{
-		$sql="delete from tab_fares where int_fare_id=".$id."";
+		$sql="delete from tab_fare where int_fare_id=".$id."";
 		$query=$this->db->query($sql);
 		return $query;
 	}
 	
 	function get_organization_details($id)
 	{
-		$sql="select * from tab_fares where int_fare_id=".$id."";
+		$sql="select * from tab_fare where int_fare_id=".$id."";
 		$query=$this->db->query($sql);
 		$result=$query->result_array();
 		return $result;
@@ -40,7 +40,7 @@ class Fare_model extends CI_Model{
 	
 	function update_org($data)
 	{
-		$sql_product="update tab_fares set float_fare='".$data['fare']."' where int_fare_id='".$data['fare_id']."'";
+		$sql_product="update tab_fare set float_fare='".$data['fare']."' where int_fare_id='".$data['fare_id']."'";
 		$query=$this->db->query($sql_product);
 		return 1;
 	}
