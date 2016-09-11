60816ab47ddf185887f05e244115a84bda65eced
Satyam <satya12100@hotmail.com>
2016-09-11 13:05:41 +0530

    all_changes

--- application/controllers/fare.php
+++ application/controllers/fare.php
@@ -6,6 +6,7 @@ class Fare extends CI_Controller{
 		$this->load->database();
 		$this->load->model('fare_model');
 		$this->load->model('location_model');
+		$this->load->model('organization_model');
 		error_reporting(E_ALL ^ (E_NOTICE | E_WARNING));
 		header('Access-Control-Allow-Origin: *');
 	}
@@ -71,6 +72,50 @@ class Fare extends CI_Controller{
 
 			$data["fares"]=$this->fare_model->get_all_fares();
 			$this->load->view('page',$data);	
+
+		}
+
+		else
+
+		{
+
+			$this->load->view('login');	
+
+		}	
+
+	}
+	
+	function fare_list_admin()
+
+	{
+
+		$user=$this->session->userdata('user');
+		$data=$this->input->post();
+
+		if(isset($user['int_user_id']) && $user['int_user_id']!='')
+
+		{
+			if(isset($data['org_id']))
+			{
+				$data1["page"]="fare_list_admin";
+				$data["fares"]=$this->fare_model->get_org_fares($data['org_id']);
+				$data1["organizations"]=$this->organization_model->get_all_organizations();
+				$data1["org_id"]=$data['org_id'];
+			}
+			else
+			{
+				$data1["page"]="fare_list_admin";
+				$data1["fares"]=array();
+				$data1["organizations"]=$this->organization_model->get_all_organizations();
+				$data1["org_id"]=NULL;
+			}
+				
+			$this->load->view('page',$data1);	
+			
+			//$data["page"]="fare_list";
+
+			//$data["fares"]=$this->fare_model->get_all_fares();
+			//$this->load->view('page',$data);	
 
 		}
 
