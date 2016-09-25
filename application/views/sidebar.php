160634f24dbc2eb91494576eaa75d1ffac5f7b9e
Satyam <satya12100@hotmail.com>
2016-09-25 22:09:10 +0530

    vehicle

--- application/views/sidebar.php
+++ application/views/sidebar.php
@@ -63,6 +63,7 @@ $user=$this->session->userdata('user');
                 <i class="fa fa-dashboard"></i> <span>Vehicle</span> <i class="fa fa-angle-left pull-right"></i>
               </a>
               <ul class="treeview-menu">
+				<li><a href="<?php echo site_url();?>/vehicle/add_admin"><i class="fa fa-circle-o"></i> Add </a></li>
                 <li class="active"><a href="<?php echo site_url();?>/vehicle/vehicle_list_admin"><i class="fa fa-circle-o"></i> View</a></li>
               </ul>
             </li>
