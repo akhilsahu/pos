137bae5ec0e2696ee83de60dc77ebe210c10702d
Satyam <satya12100@hotmail.com>
2016-09-28 13:32:21 +0530

    fare

--- application/controllers/fare.php
+++ application/controllers/fare.php
@@ -60,8 +60,7 @@ class Fare extends CI_Controller{
 		{
 			$data["page"]="import_form_admin";
 			$data["organizations"]=$this->organization_model->get_all_organizations();
-			print_r($data);exit;
-			//$this->load->view('page',$data);
+			$this->load->view('page',$data);
 		}
 		else
 		{
